7487144c0ff66e2bf329eaed85076c522039d5ba
Melvin SCHULTZ <mschultz@kaliop.com>
2016-12-08 08:02:36 -0500

    create a Place with Prices > DONE

--- src/AppBundle/Controller/PlaceController.php
+++ src/AppBundle/Controller/PlaceController.php
@@ -56,6 +56,10 @@ class PlaceController extends Controller
 
         if ($form->isValid()) {
             $em = $this->get('doctrine.orm.entity_manager');
+            foreach ($place->getPrices() as $price) {
+                $price->setPlace($place);
+                $em->persist($price);
+            }
             $em->persist($place);
             $em->flush();
             return $place;
